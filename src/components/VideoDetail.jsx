f746a5023507be090cbaaa2d1b00a9c2bc6dad25
Abuki <145156012+Abubeker09@users.noreply.github.com>
2024-01-13 16:45:33 +0300

    Update VideoDetail.jsx

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -27,8 +27,8 @@ const VideoDetail = () => {
     <Box minHeight="90vh">
       <Stack>
         <Box flex={1}>
-          <Box sx={{width:'100%', position:'sticky', top:0}}>
-            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player max-md:h-[50%]' controls/>
+          <Box sx={{width:'100%', height: '70%', position:'sticky', top:0}}>
+            <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className='react-player' controls/>
             <Typography color='#fff' variant='h5' fontWeight='bold' p={2}>
               {title}
             </Typography>
